04df4b7f6a9d9cde1b627f62d53250633a868453
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-14 15:23:50 -0800

    got the names to redner on the shopping list page

--- src/components/ShoppingListItems/ShoppingListItems.jsx
+++ src/components/ShoppingListItems/ShoppingListItems.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export default function ShoppingListItems() {
-  return <div>ShoppingListItems</div>;
+export default function ShoppingListItems({ shoppingList }) {
+  console.log({ shoppingList });
+  return (
+    <div>
+      {shoppingList.map(({ name, id, count, completed }) => (
+        <div key={id}>
+          <span>{name}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/views/ShoppingList/ShoppingList.jsx
+++ src/views/ShoppingList/ShoppingList.jsx
@@ -3,12 +3,33 @@ import AddItemForm from '../../components/AddItemForm/AddItemForm';
 import ShoppingListItems from '../../components/ShoppingListItems/ShoppingListItems';
 import style from './ShoppingList.css';
 
+const initialShoppingList = [
+  {
+    id: 1,
+    name: 'banana',
+    count: 3,
+    completed: false,
+  },
+  {
+    id: 2,
+    name: 'beans',
+    count: 500,
+    completed: false,
+  },
+  {
+    id: 3,
+    name: 'poki',
+    count: 1,
+    completed: false,
+  },
+];
+
 export default function ShoppingList() {
   return (
-    <div className={style.shoppingList}>
+    <div className={style.shoppingListView}>
       <h1>Shopping list</h1>
       <AddItemForm />
-      <ShoppingListItems />
+      <ShoppingListItems shoppingList={initialShoppingList} />
     </div>
   );
 }
