57d58398f8cde00cc4607793986042f2868e7a98
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-16 17:46:46 -0800

    just a bit of css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import ShoppingList from './views/ShoppingList/ShoppingList';
 import Header from './views/Header/Header';
+import style from './App.css';
+
 export default function App() {
   return (
-    <div>
+    <div className={style.app}>
       <Header />
       <ShoppingList />
     </div>
