68688f5cd1cebe78c74a42bee3af21fc071499ff
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-16 17:26:10 -0800

    got my app to work test failing

--- src/context/ItemContext.jsx
+++ src/context/ItemContext.jsx
@@ -1,11 +1,111 @@
 import React from 'react';
 import { useContext, createContext } from 'react';
+import { useReducer } from 'react';
 
 const ItemsContext = createContext();
 
+const initialShoppingList = [
+  {
+    id: 1,
+    name: 'banana',
+    count: 3,
+    completed: false,
+  },
+  {
+    id: 2,
+    name: 'beans',
+    count: 500,
+    completed: false,
+  },
+  {
+    id: 3,
+    name: 'poki',
+    count: 1,
+    completed: false,
+  },
+];
+
+const itemReducer = (state, { type, name, count, id }) => {
+  switch (type) {
+    case 'add':
+      return [
+        ...state,
+        { name, count, id, completed: false, isEditing: false },
+      ];
+
+    case 'delete':
+      const newState = state.filter((item) => {
+        return item.id !== Number(id);
+      });
+      return newState;
+
+    case 'edit':
+      const newEditState = state.map((item) => {
+        if (item.id === Number(id)) {
+          item.isEditing = true;
+        }
+        return item;
+      });
+      return newEditState;
+
+    case 'save':
+      const newSaveState = state.filter((item) => {
+        return item.id !== Number(id);
+      });
+      return [
+        ...newSaveState,
+        { name, count, id, completed: false, isEditing: false },
+      ];
+
+    default:
+      throw new Error(`type ${type} is not a valid type`);
+  }
+};
+
 export const ItemsProvider = ({ children }) => {
-  const test = ' you got it!';
-  return <ItemsContext.Provider value={test}>{children}</ItemsContext.Provider>;
+  const [itemList, dispatch] = useReducer(itemReducer, initialShoppingList);
+
+  const addNewItem = (name, count) => {
+    dispatch({
+      type: 'add',
+      name,
+      count,
+      id: itemList.length + 1,
+    });
+  };
+
+  const handleDelete = (id) => {
+    dispatch({
+      type: 'delete',
+      id,
+    });
+  };
+  const handleEdit = (id) => {
+    dispatch({
+      type: 'edit',
+      id,
+    });
+  };
+  const handleSaveEdit = (name, count, id) => {
+    dispatch({
+      type: 'save',
+      name,
+      count,
+      id,
+    });
+  };
+  const providerValue = {
+    handleDelete,
+    handleEdit,
+    handleSaveEdit,
+    addNewItem,
+    itemList,
+  };
+  return (
+    <ItemsContext.Provider value={providerValue}>
+      {children}
+    </ItemsContext.Provider>
+  );
 };
 
 export const useItems = () => {
--- src/views/ShoppingList/ShoppingList.jsx
+++ src/views/ShoppingList/ShoppingList.jsx
@@ -2,97 +2,11 @@ import React, { useReducer } from 'react';
 import AddItemForm from '../../components/AddItemForm/AddItemForm';
 import ShoppingListItems from '../../components/ShoppingListItems/ShoppingListItems';
 import style from './ShoppingList.css';
-
-const initialShoppingList = [
-  {
-    id: 1,
-    name: 'banana',
-    count: 3,
-    completed: false,
-  },
-  {
-    id: 2,
-    name: 'beans',
-    count: 500,
-    completed: false,
-  },
-  {
-    id: 3,
-    name: 'poki',
-    count: 1,
-    completed: false,
-  },
-];
-
-const itemReducer = (state, { type, name, count, id }) => {
-  switch (type) {
-    case 'add':
-      return [
-        ...state,
-        { name, count, id, completed: false, isEditing: false },
-      ];
-
-    case 'delete':
-      const newState = state.filter((item) => {
-        return item.id !== Number(id);
-      });
-      return newState;
-
-    case 'edit':
-      const newEditState = state.map((item) => {
-        if (item.id === Number(id)) {
-          item.isEditing = true;
-        }
-        return item;
-      });
-      return newEditState;
-
-    case 'save':
-      const newSaveState = state.filter((item) => {
-        return item.id !== Number(id);
-      });
-      return [
-        ...newSaveState,
-        { name, count, id, completed: false, isEditing: false },
-      ];
-
-    default:
-      throw new Error(`type ${type} is not a valid type`);
-  }
-};
+import { useItems } from '../../context/ItemContext';
 
 export default function ShoppingList() {
-  const [itemList, dispatch] = useReducer(itemReducer, initialShoppingList);
-
-  const addNewItem = (name, count) => {
-    dispatch({
-      type: 'add',
-      name,
-      count,
-      id: itemList.length + 1,
-    });
-  };
-
-  const handleDelete = (id) => {
-    dispatch({
-      type: 'delete',
-      id,
-    });
-  };
-  const handleEdit = (id) => {
-    dispatch({
-      type: 'edit',
-      id,
-    });
-  };
-  const handleSaveEdit = (name, count, id) => {
-    dispatch({
-      type: 'save',
-      name,
-      count,
-      id,
-    });
-  };
+  const { handleDelete, handleEdit, handleSaveEdit, itemList, addNewItem } =
+    useItems();
 
   return (
     <div className={style.shoppingListView}>
